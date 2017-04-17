db64553f0d628e10228f3639a0e0c3b9762833f3
Thomas Lundquist <github@bisonlab.no>
2017-04-17 15:49:01 +0200

    Files forgotten.

--- bin/prep.sh
+++ bin/prep.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-files="composer.json app/AppKernelCustomTrait.php app/config/states.yml app/config/contexts.yml app/config/types.yml app/config/config_custom.yml"
+files="composer.json app/AppKernelCustomTrait.php app/config/states.yml app/config/contexts.yml app/config/types.yml app/config/config_custom.yml app/config/services.yml routing_custom.yml.dist"
 
 for f in $files
  do
