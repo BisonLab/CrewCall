c134d963a45580784eeabb710f1636ef42f621f6
Thomas Lundquist <github@bisonlab.no>
2018-01-31 11:20:47 +0000

    No superadmin, makes a mess.

--- bin/reload.sh
+++ bin/reload.sh
@@ -5,7 +5,7 @@ rm -rf var/cache/*
 ./bin/console --force  --em=sakonnin doctrine:schema:drop
 ./bin/console doctrine:schema:create
 ./bin/console --em=sakonnin doctrine:schema:create
-./bin/console fos:user:create --super-admin crewcall crewcall@localhost cc
+./bin/console fos:user:create --admin crewcall crewcall@localhost cc
 ./bin/console cache:clear --no-warmup
 ./bin/console sakonnin:insert-basedata
 ./bin/console once:create-base-data
