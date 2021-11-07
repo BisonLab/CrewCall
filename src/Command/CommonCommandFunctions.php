b7dbe0058d92c1146212c1150247ec20b0d6714e
Thomas Lundquist <github@bisonlab.no>
2021-11-07 15:12:13 +0100

    Gotta get the entity managers correctly.

--- src/Command/CommonCommandFunctions.php
+++ src/Command/CommonCommandFunctions.php
@@ -10,6 +10,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
@@ -17,15 +18,17 @@ use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 trait CommonCommandFunctions
 {
     private $params;
+    private $manager_registry;
     private $crewcall_em;
     private $sakonnin_em;
     private $passwordEncoder;
 
-    public function __construct(EntityManagerInterface $crewcall_em, EntityManagerInterface $sakonnin_em, UserPasswordEncoderInterface $passwordEncoder, ParameterBagInterface $params)
+    public function __construct(ManagerRegistry $manager_registry, UserPasswordEncoderInterface $passwordEncoder, ParameterBagInterface $params)
     {
         $this->params = $params;
-        $this->crewcall_em = $crewcall_em;
-        $this->sakonnin_em = $sakonnin_em;
+        $this->manager_registry = $manager_registry;
+        $this->crewcall_em = $manager_registry->getManager('crewcall');
+        $this->sakonnin_em = $manager_registry->getManager('sakonnin');
         $this->passwordEncoder = $passwordEncoder;
 
         parent::__construct();
