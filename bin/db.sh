e73c01afa65686558825b4d9635c723aaa02fdcb
Thomas Lundquist <github@bisonlab.no>
2022-09-13 16:18:26 +0200

    Just connect via localhost

--- bin/db.sh
+++ bin/db.sh
@@ -3,9 +3,7 @@
 user=$(grep "^DATABASE_URL=" .env.local | perl -p -e 's/.*\/\/([\w-_]+):.*/$1/')
 
 pass=$(grep "^DATABASE_URL=" .env.local | perl -p -e 's/.*\/\/([\w-_]+):(.*)@.*/$2/')
-echo $pass
 
 name=$(grep "^DATABASE_URL=" .env.local | perl -p -e 's/.*\w\/([\w-_]+)\?ser.*/$1/')
 
-echo psql -U $user $name
-psql -U $user $name
+PGPASSWORD=$pass psql -h localhost -U $user $name
