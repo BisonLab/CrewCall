ec42ed696c61abcc235fea1df7bd82f0dc3046df
Thomas Lundquist <github@bisonlab.no>
2022-11-07 21:51:36 +0100

    Configureable person fields, mkII

--- src/Form/PersonType.php
+++ src/Form/PersonType.php
@@ -23,39 +23,42 @@ class PersonType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $personfields = $options['personfields'];
         $builder
             ->add('username')
             ->add('email')
             ->add('first_name')
             ->add('last_name')
         ;
-        // Annoying, but working. 
-        if (in_array('date_of_birth', $options['personfields']))
+        if ($personfields['date_of_birth']['enabled'])
             $builder->add('date_of_birth', BirthdayType::class, array('required' => false));
 
-        if (in_array('nationality', $options['personfields']))
+        if ($personfields['nationality']['enabled'])
             $builder->add('nationality', CountryType::class, array(
                 'alpha3' => true, 'required' => false));
 
-        if (in_array('emergency_contact', $options['personfields']))
+        if ($personfields['emergency_contact']['enabled'])
             $builder->add('emergency_contact', TextareaType::class, array('required' => false));
 
-        if (in_array('workload_percentage', $options['personfields']))
+        if ($personfields['workload_percentage']['enabled'])
             $builder->add('workload_percentage', NumberType::class, array('required' => false));
 
-        if (in_array('diets', $options['personfields']))
+        if ($personfields['diets']['enabled'])
             $builder->add('diets', ChoiceType::class,array(
                 'choices' => ExternalEntityConfig::getTypesAsChoicesFor('Person', 'Diet'),
                 'expanded'  => true,
                 'multiple'  => true,));
 
-        if (in_array('', $options['personfields']))
+        if ($personfields['mobile_phone_number']['enabled'])
             $builder->add('mobile_phone_number');
 
-        if (in_array('home_phone_number', $options['personfields']))
+        if ($personfields['home_phone_number']['enabled'])
             $builder->add('home_phone_number');
 
-        $builder->add('state', ChoiceType::class, array('label' => 'Status', 'choices' => ExternalEntityConfig::getStatesAsChoicesFor('Person')));
+        $builder->add('state', ChoiceType::class, [
+            'label' => 'Status',
+            'choices' => ExternalEntityConfig::getStatesAsChoicesFor('Person')
+            ]);
 
         $builder->add('system_roles', ChoiceType::class,
             array(
@@ -64,11 +67,12 @@ class PersonType extends AbstractType
                 'choices' => ExternalEntityConfig::getSystemRolesAsChoices('with_description'),
             )
         );
-        if (in_array('address', $options['personfields']))
-            $builder->add('address', AddressType::class, ['address_elements' => $options['address_elements']]);
+        if ($personfields['address']['enabled'])
+            $builder->add('address', AddressType::class, [
+                'address_elements' => $options['address_elements']]);
 
         // Butter and lard.
-        if (in_array('postal_address', $options['personfields']) && $options['addressing_config']['use_postal_address']) {
+        if ($personfields['postal_address']['enabled'] && $options['addressing_config']['use_postal_address']) {
             $builder->add('postal_address', AddressType::class, ['address_elements' => $options['address_elements']])
             ;
         }
