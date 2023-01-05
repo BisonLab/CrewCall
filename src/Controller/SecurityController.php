c22c766a3f3ea173a9431843e86f3de49cc41027
Thomas Lundquist <github@bisonlab.no>
2023-01-05 14:26:07 +0100

    Belt and suspenders.

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Doctrine\ORM\EntityManagerInterface;
 
 use BisonLab\CommonBundle\Controller\CommonController as CommonController;
 
@@ -14,9 +15,11 @@ class SecurityController extends CommonController
     /**
      * @Route("/login", name="app_login")
      */
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils, EntityManagerInterface $entityManager): Response
     {
         if ($user = $this->getUser()) {
+            $user->setLastLogin(new \DateTime());
+            $entityManager->flush();
             if ($user->isAdmin())
                 return $this->redirectToRoute('dashboard');
             else
