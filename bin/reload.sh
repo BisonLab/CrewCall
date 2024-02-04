dff37eae2ab47203c0aae7225e4c4e13de285391
Thomas Lundquist <github@bisonlab.no>
2024-02-04 15:07:23 +0100

    Make this webhostingfriendly.

--- bin/reload.sh
+++ bin/reload.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
 rm -rf var/cache/*
-./bin/console --force  doctrine:schema:drop
-./bin/console --force  --em=sakonnin doctrine:schema:drop
-./bin/console doctrine:schema:create
-./bin/console --em=sakonnin doctrine:schema:create
-./bin/console cache:clear --no-warmup
-./bin/console once:create-base-data
+php ./bin/console --force  doctrine:schema:drop
+php ./bin/console --force  --em=sakonnin doctrine:schema:drop
+php ./bin/console doctrine:schema:create
+php ./bin/console --em=sakonnin doctrine:schema:create
+php ./bin/console cache:clear --no-warmup
+php ./bin/console once:create-base-data
 if [ "$1" = "with-user" ]
  then
     [ -n "$2" ] && user=$2
     [ -n "$3" ] && email=$3
-    ./bin/console crewcall:user:create --role=ADMIN $user $email
+    php ./bin/console crewcall:user:create --role=ADMIN $user $email
     echo Created user $user. Sending password email to $email 
-    ./bin/console crewcall:user:send-passwordmail $user
+    php ./bin/console crewcall:user:send-passwordmail $user
 fi
 
 #rm -rf var/cache/*
